838bb5d0a7c40dab91f000b9f56df8a978452016
Gio1109 <89951244+Gio1109@users.noreply.github.com>
2021-10-04 20:03:13 -0700

    commit

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,57 @@
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Giovanni Guzman" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Ginger bread
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
+let total = 0   //total
 
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Ginger Bread cookies
+//ginger bread
 document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
+
     console.log('Ginger bread + button was clicked!')
 
-    // TODO: Write the code to be run when the "+" button for "Ginger Bread" is clicked
+  gb = gb + 1
+document.getElementById('qty-gb').textContent = gb
+total = total + 1
+document.getElementById('qty-total').textContent = total
 })
-
-// TODO: Hook up event listeners for the rest of the buttons
+document.getElementById('minus-gb').addEventListener('click', function() {
+    gb = gb -1
+    document.getElementById('qty-gb').textContent = gb
+    total = total + 1
+    document.getElementById('qty-total').textContent = total
+})
+// Chocolate Chip
+document.getElementById('add-cc').addEventListener('click', function(){
+    cc = cc + 1;
+    document.getElementById('qty-cc').textContent = cc;
+    total = total + 1;
+    document.getElementById('qty-total').textContent = total;
+})
+document.getElementById('minus-cc').addEventListener('click', function(){
+    cc = cc - 1;
+    document.getElementById('qty-cc').textContent = cc;
+    total = total - 1;
+    document.getElementById('qty-total').textContent = total;
+})
+// Sugar cookie
+document.getElementById('add-sugar').addEventListener('click', function(){
+    sugar = sugar + 1;
+    document.getElementById('qty-sugar').textContent = sugar;
+    total = total + 1;
+    document.getElementById('qty-total').textContent = sugar;
+})
+document.getElementById('minus-sugar').addEventListener('click', function(){
+    sugar = sugar - 1;
+    document.getElementsById('qty-sugar').textContent = sugar;
+    total = total - 1;
+    document.getElementById('qty-total').textContent = sugar
+})
